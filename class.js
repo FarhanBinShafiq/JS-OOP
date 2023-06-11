105520001549b492c31bb140dc42104ba9de3dc3
farhanbshafiq <farhanbinshafiq@gmail.com>
2023-06-11 12:50:25 +0600

    class method

--- class.js
+++ class.js
@@ -1,15 +1,36 @@
-function Person(name,email){
-    this.name=name,
-    this.email=email
+// function Person(name,email){
+//     this.name=name,
+//     this.email=email
+// }
+
+// var x=new Person('Farhan','Shafiq@gmail.com')
+// var y=new Person('Mishu','Mishu@gmail.com')
+// var people=[x,y]
+// console.log(people)
+
+
+// people.forEach((p)=>{
+//     console.log(`name :` + p.name)
+// })
+
+
+class Person{
+    constructor(name,email,dob,phone){
+        this.name=name;
+        this.email=email;
+        this.dob=dob;
+        this.phone=phone
+    }
+
+      getPersonDetail(name,email,dob,phone){
+         console.log (`Hello,  I am ${this.name}.
+         My email is ${this.email}.
+         Date of Birth ${this.dob}.
+         Phone ${this.phone}`)
+      }
 }
 
-var x=new Person('Farhan','Shafiq@gmail.com')
-var y=new Person('Mishu','Mishu@gmail.com')
-var people=[x,y]
-console.log(people)
 
-
-people.forEach((p)=>{
-    console.log(`name :` + p.name)
-})
-
+const persondetail= new Person('Farhan',"farhan10237@gmail.com",'8/8/1963','013695244852')
+console.log(persondetail)
+persondetail.getPersonDetail()
--- object.js
+++ object.js
@@ -1,14 +1,14 @@
-var book={
-    name:"Math",
-    Author:"XYZ",
-    print:function(){
-          console.log(this.name, this.Author)
-    },
-    page:250
-}
+// var book={
+//     name:"Math",
+//     Author:"XYZ",
+//     print:function(){
+//           console.log(this.name, this.Author)
+//     },
+//     page:250
+// }
 
-console.log(book)
+// console.log(book)
 
-book.rating=4.00
-console.log(book)
-console.log(book.print())
+// // book.rating=4.00
+// // console.log(book)
+// // console.log(book.print())
